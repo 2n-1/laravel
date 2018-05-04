af3ef5375e618e4a6d73ad0c5bf73368c0ae142a
ilcapo1 <s201428@studenti.polito.it>
2018-05-04 21:33:24 +0200

    progress

--- scripts/db-connect.sh
+++ scripts/db-connect.sh
@@ -1,5 +1,5 @@
 dir=$(pwd)
-project=$(echo $dir | tr "/" "\n" | tail -1 | tr '[:upper:]' '[:lower:]'
+project=$(echo $dir | tr "/" "\n" | tail -1 | tr '[:upper:]' '[:lower:]')
 
 ip=$(docker inspect ${project}_database_1 | grep IPAddress | tail -1 | tr -s ' ' | tr -d ',' | cut -d' ' -f3 | tr -d '"')
 
